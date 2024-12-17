4faed3aa078631e97910c1021ea81c60ca271da5
muthutm1701 <muthutm1701@gmail.com>
2024-12-17 21:55:07 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ const app = express();
 app.use("/helo",(req,res)=>{
     res.send("hello from server");
 });
-app.listen(3000,()=>{
-    console.log("server listened at port 3000");
+app.listen(4000,()=>{
+    console.log("server listened at port 2000");
 });
